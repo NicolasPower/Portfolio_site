0516aec7d59e3f755c7473e7e6e82e209f33bde3
NicolasPower <nicolas.power@optusnet.com.au>
2023-11-15 13:51:24 +1000

    started setting up layout

--- portfolio_site/src/app/page.tsx
+++ portfolio_site/src/app/page.tsx
@@ -1,113 +1,80 @@
-import Image from 'next/image'
+/* eslint-disable @next/next/no-img-element */
+"use client"
 
-export default function Home() {
+import React, { useState } from 'react';
+import Image from "next"
+
+const Home: React.FC = () => {
+  const [activeSection, setActiveSection] = useState<string>('about'); // Default to 'about' section
+
+  const handleButtonClick = (section: string) => {
+    setActiveSection(section);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col justify-center items-center w-4/5 mx-auto mt-20">
+
+      {/* Header */}
+      <div>
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Hi, I&apos;m Nicolas Power</h1>
+      </div>
+
+      {/* Topic Buttons */}
+      <div className="flex flex-row justify-center content-center items-center w-full mx-auto mt-20">
+        <div className={`flex-initial text-center basis-1/4 cursor-pointer ${activeSection === 'about' ? 'font-bold' : ''}`} onClick={() => handleButtonClick('about')}>
+          <p>About</p>
+        </div>
+
+        <div className={`flex-initial text-center basis-1/4 cursor-pointer ${activeSection === 'projects' ? 'font-bold' : ''}`} onClick={() => handleButtonClick('projects')}>
+          <p>Projects</p>
+        </div>
+
+        <div className={`flex-initial text-center basis-1/4 cursor-pointer ${activeSection === 'skills' ? 'font-bold' : ''}`} onClick={() => handleButtonClick('skills')}>
+          <p>Skills</p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* Content */}
+      <div>
+        {activeSection === 'about' && (
+          <div>
+            <p className='pt-2 text-center'>
+              I am a proactive, engaging and motivated university student studying a Bachelor of Information Technology at
+              Queensland University of Technology seeking to get some work experience while I study. Working in a wide array job
+              has given me a strong and reliable work ethic. I am a great communicator who works incredibly well in a team setting
+              and who is always eager to learn new skills.
+            </p>
+            <p className='pt-2 text-center'>
+              Having worked in a variety of customer service and labour jobs has given me great insight into working as a team in a
+              professional setting as well as how to take initiative when unexpected circumstances arise.
+              With the goal to develop my professional skills I am looking for a place to expand my career skills and further grow
+              my network. I look forward to working with you.
+            </p>
+          </div>
+        )}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        {activeSection === 'projects' && (
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/img/food-name.png" alt="" />
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Musical Foods</h5>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    For an Assignment we were tasked to develop a stateless web application. I create an application that took the user's First and last name and provide them with a food and playlist from the names origin.
+                  </p>
+              </div>
+          </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        )}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {activeSection === 'skills' && (
+          <div>
+            <p>Skills section</p>
+          </div>
+        )}
       </div>
     </main>
-  )
-}
+  );
+};
+
+export default Home;
