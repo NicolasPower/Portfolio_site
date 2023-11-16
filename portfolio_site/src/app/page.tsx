1173164474ee04338a9f19b68a889f660259567a
NicolasPower <nicolas.power@optusnet.com.au>
2023-11-16 13:30:56 +1000

    added first skills draft

--- portfolio_site/src/app/page.tsx
+++ portfolio_site/src/app/page.tsx
@@ -35,7 +35,7 @@ const Home: React.FC = () => {
       </div>
 
       {/* Content */}
-      <div>
+      <div className='w-full'>
         {activeSection === 'about' && (
           <div>
             <p className='pt-2 text-center'>
@@ -54,22 +54,155 @@ const Home: React.FC = () => {
         )}
 
         {activeSection === 'projects' && (
-
-          <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/img/food-name.png" alt="" />
-              <div className="flex flex-col justify-between p-4 leading-normal">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Musical Foods</h5>
-                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    For an Assignment we were tasked to develop a stateless web application. I create an application that took the user's First and last name and provide them with a food and playlist from the names origin.
-                  </p>
+          <div className='flex flex-row pt-2'>
+            <div className='p-2'>
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                  <a href="#">
+                      <img className="rounded-t-lg" src="/img/food-name.png" alt="" />
+                  </a>
+                  <div className="p-5">
+                      <a href="#">
+                          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Musical Foods</h5>
+                      </a>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        This web application ustilises API's to find the origin of the users name and provide them with a geographically appropiate recipe and playlist.
+                      </p>
+                      <a href="https://github.com/NicolasPower/food-name.git" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                          Github
+                          <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                          </svg>
+                      </a>
+                  </div>
               </div>
-          </a>
+            </div>
+
+            <div className='p-2'>
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                  <a href="#">
+                      <img className="rounded-t-lg" src="/img/food-name.png" alt="" />
+                  </a>
+                  <div className="p-5">
+                      <a href="#">
+                          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Musical Foods</h5>
+                      </a>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        This web application ustilises API's to find the origin of the users name and provide them with a geographically appropiate recipe and playlist.
+                      </p>
+                      <a href="https://github.com/NicolasPower/food-name.git" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                          Github
+                          <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                          </svg>
+                      </a>
+                  </div>
+              </div>
+            </div>
+
+            <div className='p-2'>
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                  <a href="#">
+                      <img className="rounded-t-lg" src="/img/food-name.png" alt="" />
+                  </a>
+                  <div className="p-5">
+                      <a href="#">
+                          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Musical Foods</h5>
+                      </a>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        This web application ustilises API's to find the origin of the users name and provide them with a geographically appropiate recipe and playlist.
+                      </p>
+                      <a href="https://github.com/NicolasPower/food-name.git" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                          Github
+                          <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                          </svg>
+                      </a>
+                  </div>
+              </div>
+            </div>
+
+          </div>
+
+
 
         )}
 
         {activeSection === 'skills' && (
-          <div>
-            <p>Skills section</p>
+          <div className='flex flex-col w-3/5 mx-auto'>
+            <div className='flex flex-wrap justify-center content-center items-center w-full mx-auto p-2'>
+            
+              <div className='flex-initial text-center w-20 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/python.png" alt=""/>
+                <p>Python</p>
+              </div>
+
+              <div className='flex-initial text-center w-24 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/c.png" alt=""/>
+                <p className=''>C</p>
+              </div>
+
+              <div className='flex-initial text-center w-24 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/c++.png" alt=""/>
+                <p className=''>C++</p>
+              </div>
+
+              <div className='flex-initial text-center w-24 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/javascript.png" alt=""/>
+                <p className=''>Java Script</p>
+              </div>
+
+              <div className='flex-initial text-center w-24 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/typescript.png" alt=""/>
+                <p className=''>Type Script</p>
+              </div>
+
+              <div className='flex-initial text-center w-24 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/react.png" alt=""/>
+                <p>React</p>
+              </div>
+
+              <div className='flex-initial text-center w-24 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/next.png" alt=""/>
+                <p className=''>Next.js</p>
+              </div>
+
+              <div className='flex-initial text-center w-24 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/flask.png" alt=""/>
+                <p className=''>flask</p>
+              </div>
+
+              <div className='flex-initial text-center w-24 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/node.png" alt=""/>
+                <p className=''>Node</p>
+              </div>
+
+              <div className='flex-initial text-center w-24 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/sql.svg" alt=""/>
+                <p className=''>SQL</p>
+              </div>
+
+              <div className='flex-initial text-center w-24 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/css.png" alt=""/>
+                <p className=''>CSS</p>
+              </div>
+
+              <div className='flex-initial text-center w-24 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/bootstrap.png" alt=""/>
+                <p className=''>Boot Strap</p>
+              </div>
+
+              <div className='flex-initial text-center w-24 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/tailwind.webp" alt=""/>
+                <p className=''>Tailwind CSS</p>
+              </div>
+
+              <div className='flex-initial text-center w-24 h-25 m-3'>
+                <img className='w-20 h-20 mx-auto' src="/img/docker.png" alt=""/>
+                <p className=''>Docker</p>
+              </div>
+
+            </div>
+
           </div>
         )}
       </div>
