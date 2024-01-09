1c45ee7b71ec822e2d94ef9ade6b6edb8bed3453
NicolasPower <nicolas.power@optusnet.com.au>
2024-01-09 15:07:59 +1000

    updated email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@
 import React, { useState } from "react";
 import { Link, Element } from "react-scroll";
 
-
-
 const Home: React.FC = () => {
   const [activeSection, setActiveSection] = useState<string>("about"); // Default to 'about' section
 
@@ -425,7 +423,7 @@ const Home: React.FC = () => {
                     <div className="flex flex-col sm:flex-row my-3 items-center w-full">
                       <svg width="75px" height="75px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)matrix(1, 0, 0, 1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.144"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M20 4C21.6569 4 23 5.34315 23 7V17C23 18.6569 21.6569 20 20 20H4C2.34315 20 1 18.6569 1 17V7C1 5.34315 2.34315 4 4 4H20ZM19.2529 6H4.74718L11.3804 11.2367C11.7437 11.5236 12.2563 11.5236 12.6197 11.2367L19.2529 6ZM3 7.1688V17C3 17.5523 3.44772 18 4 18H20C20.5523 18 21 17.5523 21 17V7.16882L13.8589 12.8065C12.769 13.667 11.231 13.667 10.1411 12.8065L3 7.1688Z" fill="#204969"></path> </g></svg>
                       <div className="flex flex-row text-start items-center px-8">
-                        <p className="text-text font-bold text-md md:text-2xl">Nicolas.Power@optusnet.com.au</p>
+                        <p className="text-text font-bold text-md md:text-2xl">Nicolasrpower@gmail.com</p>
                       </div>
                     </div>
 
